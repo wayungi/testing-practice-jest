4465b2f9e94bdee592fb5d0637170d6f6ff6c2da
Francis <wayncis2@gmail.com>
2022-05-18 11:17:48 +0300

    Add capitalize tests

--- capitalize.test.js
+++ capitalize.test.js
@@ -1,12 +1,14 @@
+const capitalize =  require('./capitalize.js');
+
+
 test("micro should retrun Micro", () => {
-  expect(capitalze('micro')).toMatch(/Micro/);
+  expect(capitalize('micro')).toMatch(/Micro/);
 });
 
-
 test("1234 should retrun 1234", () => {
-  expect(capitalze('micro')).toMatch(/1234/);
+  expect(capitalize('1234')).toMatch(/1234/);
 });
 
 test("MICROVERSE should retrun Microverse", () => {
-  expect(capitalze('MICROVERSE')).toMatch(/Microverse/);
+  expect(capitalize('MICROVERSE')).toMatch(/Microverse/);
 });
